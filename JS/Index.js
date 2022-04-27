972a5b1803810fbf096b1a447d0d5a262fc6588e
Sayali <sayalipanch17@gmail.com>
2022-04-27 14:33:01 +0530

    Integrated Firebase for login page

---
+++ JS/Index.js
@@ -0,0 +1,20 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+const auth = getAuth();
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+  }
+});
+
+function login(){
+    var userEmail = document.getElementById("email").value;
+    var userPass = document.getElementById("pass").value;
+
+}
